71a2dfb9e0352ba917d63dcb12ebdc9852250781
Ali Yousuf <aly.yousuf7@gmail.com>
2019-01-15 17:28:56 +0500

    Add restify server for creating session
    Signed-off-by: Ali Yousuf <aly.yousuf7@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,30 +1,36 @@
+const restify = require('restify');
 const VitalChat = require('vitalchat');
 
-const client = new VitalChat({
-    key: 'test',
-    secret: 'dfff6d37-4a7f-4d4d-9d48-14b6e8958ecb',
-    baseUrl: 'https://signal-local.vitalchat.com:1443'
+const PORT = parseInt(process.env['PORT']) || 3000;
+const server = restify.createServer();
+
+server.get('/create_session', (req, res, next) => {
+    const client = new VitalChat({
+        key: 'test',
+        secret: 'dfff6d37-4a7f-4d4d-9d48-14b6e8958ecb',
+        baseUrl: 'https://signal-local.vitalchat.com:1443'
+    });
+
+    client.createSession({
+        tags: [{ my_id: '1234'}, { reference: '5678'}],
+        callback: { url: 'https://myserver.com/vitalchat', user: '', password: ''},
+        defaults: { character: 'sally' }
+    })
+        .then((session) => {
+            session.on('connect', (session_id) => {
+                console.log(session_id, 'connected');
+
+                res.send({ session_id });
+            });
+            session.on('disconnect', (session_id) => {
+                console.log(session_id, 'disconnected');
+            });
+            session.on('message', (message) => {
+                console.log(message.session_id, 'message:', message.data);
+            });
+        });
 });
 
-client.createSession({
-    tags: [{ my_id: '1234'}, { reference: '5678'}],
-    callback: { url: 'https://myserver.com/vitalchat', user: '', password: ''},
-    defaults: { character: 'sally' }
-})
-    .then((session) => {
-        session.on('connect', (session_id) => {
-            console.log(session_id, 'connected');
-
-            session.init();
-        });
-        session.on('disconnect', (session_id) => {
-            console.log(session_id, 'disconnected');
-        });
-        session.on('message', (message) => {
-            console.log(message.session_id, 'message:', message.data);
-        });
-
-        setTimeout(() => {
-            session.close();
-        }, 5000);
-    });
+server.listen(PORT, () => {
+    console.log('%s listening at %s', server.name, server.url);
+});

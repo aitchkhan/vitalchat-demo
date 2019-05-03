0cfd479541b84782bfe0b39c0071f607d0ec30d7
Ali Yousuf <aly.yousuf7@gmail.com>
2019-05-03 18:24:57 +0500

    Use new image generator and change few text
    Signed-off-by: Ali Yousuf <aly.yousuf7@gmail.com>

--- server/index.js
+++ server/index.js
@@ -32,7 +32,8 @@ server.get('/api/create_session', (req, res) => {
                 });
 
                 function imagePath(text) {
-                    return `https://dummyimage.com/640x360/000000/fff.jpg&text=${encodeURIComponent(text)}`;
+                    text = text.replace(/\n/gu, '_br_');
+                    return `https://imgplaceholder.com/640x360/000?font-size=25&font-family=OpenSans&text=${encodeURIComponent(text)}`;
                 }
 
                 // Try to recognize with in 10 seconds
@@ -44,53 +45,53 @@ server.get('/api/create_session', (req, res) => {
 
                     .then(() => conversation.speak('Which of these can I help with? Please say one of these words.'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Symptom Checker 2. Medical Records')))
+                    .then(() => conversation.showImage(imagePath('1. Symptom Checker\n2. Medical Records')))
                     .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['symptom', 'symptoms'])))
 
                     .then(() => conversation.speak('Please say one of the following chief complaints.'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Headache 2. Diarrhea')))
+                    .then(() => conversation.showImage(imagePath('1. Headache\n2. Diarrhea')))
                     .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['headache', 'headaches', 'diarrhea'])))
 
                     .then(() => conversation.speak('Which of the following best describes your pain level?'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Extreme 2. Mild to moderate 3. Moderate to severe 4. Pressure or squeezing sensation 5. Stabbing or burning 6. Throbbing')))
+                    .then(() => conversation.showImage(imagePath('1. Extreme\n2. Mild to moderate\n3. Moderate to severe\n4. Pressure or squeezing sensation\n5. Stabbing or burning\n6. Throbbing')))
                     .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['extreme', 'mild', 'moderate', 'severe', 'pressure', 'squeezing', 'sensation', 'stabbing', 'burning', 'throbbing'])))
 
                     .then(() => conversation.speak('Where is the pain located?'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Around one eye or radiates from one eye 2. Around your temples 3. On both sides of your head 4. On one side of your head')))
+                    .then(() => conversation.showImage(imagePath('1. Around one eye or radiates from one eye\n2. Around your temples\n3. On both sides of your head\n4. On one side of your head')))
                     .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['eye', 'temple', 'temples', 'side', 'head'])))
 
                     .then(() => conversation.speak('How did the pain onset?'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Gradual 2. Preceded by a head injury or fall 3. Preceded by frequent use of pain medication 4. Preceded by visual or other sensory disturbances 5. Sudden')))
+                    .then(() => conversation.showImage(imagePath('1. Gradual\n2. Preceded by a head injury or fall\n3. Preceded by frequent use of pain medication\n4. Preceded by visual or other sensory disturbances\n5. Sudden')))
                     .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['gradual', 'head', 'injury', 'fall', 'pain', 'medication', 'visual', 'sensory', 'disturbance', 'sudden'])))
 
                     .then(() => conversation.speak('How often do you get these headaches?'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Gradually becomes more frequent 2. Is daily 3. Is often the same time every day')))
+                    .then(() => conversation.showImage(imagePath('1. Gradually becomes more frequent\n2. Is daily\n3. Is often the same time every day')))
                     .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['frequent', 'daily', 'time', 'every', 'day', 'everyday'])))
 
                     .then(() => conversation.speak('Is the pain triggered or worsened by any of these?'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Change in sleep patterns 2. Chewing 3. Clenching or grinding teeth 4. Everyday activities 5. Hormonal changes 6. Orgasm 7. Poor posture 8. Stress')))
-                    .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['sleep', 'pattern', 'patterns', 'chewing', 'chew', 'clenching', 'clench', 'grinding', 'grind', 'teeth', 'activities', 'activity', 'hormonal', 'orgasm', 'posture', 'stress'])))
+                    .then(() => conversation.showImage(imagePath('1. Change in sleep patterns\n2. Chewing\n3. Clenching or grinding teeth\n4. Everyday activities\n5. Hormonal changes\n6. Poor posture\n7. Stress')))
+                    .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['sleep', 'pattern', 'patterns', 'chewing', 'chew', 'clenching', 'clench', 'grinding', 'grind', 'teeth', 'activities', 'activity', 'hormonal', 'posture', 'stress'])))
 
                     .then(() => conversation.speak('Does the pain get relieved by any of these?'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.showImage(imagePath('1. Lying down in the dark 2. Over-the-counter pain medication 3. Rest')))
+                    .then(() => conversation.showImage(imagePath('1. Lying down in the dark\n2. Over-the-counter pain medication\n3. Rest')))
                     .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['lying', 'dark', 'counter', 'pain', 'medication', 'rest'])))
 
                     .then(() => conversation.speak('Thank you for answering all the questions. It is possible that you have Tension Headache. Would you like to see a video about it?'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()))
-                    .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['yes', 'yep', 'sure'])))
+                    .then(() => conversation.waitUntil(ConvoController.recognizeSpeech(['yes', 'yep', 'sure', 'no'])))
 
                     .then(() => conversation.playVideo('file:///mcu/assets/media/video.webm'))
                     .then(() => conversation.waitUntil((message) => ConvoController.videoEnd()(message) || ConvoController.recognizeSpeech('stop')(message)))
                     .then(() => conversation.clearMedia())
 
-                    .then(() => conversation.speak('Thank you'))
+                    .then(() => conversation.speak('Thank you for using the Symptom checker'))
                     .then(() => conversation.waitUntil(ConvoController.speechEnd()));
             });
 
